905613e062c36b614df495b2183cfc73b5a9daa3
shabobble <pksullivan@yahoo.com>
2021-04-08 18:19:03 -0400

    final touches

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,15 @@
-  
-var appointText = "";
-var appointTime = "";
-var currentDate;
-var currentTime;
-var currentContainer;
-var tempArray = [];
-var storedAppointments;
-var returnedAppointments;
+let appointText = "";
+let appointTime = "";
+let currentTime;
+let currentContainer;
+let dataArray = [];
+let storedAppointments;
+let returnedAppointments;
 
 let currentDay = $("#currentDay");
+let fivePM = $('#five-pm');
+let textArea = $('#textarea');
+let saveButton = $('.saveBtn');
 
 function updateTime() {
     currentDay.text(moment().format("MMM DD, YYYY hh:mm:ss a"))
@@ -20,57 +21,160 @@ setInterval(function() {
     updateTime();
 },1000)
 
-//     function renderAppointments() {
-//         storedAppointments = JSON.parse(localStorage.getItem("appointments"));
-//         if (storedAppointments !== null) {
-//             for (i = 0; i < storedAppointments.length; i++) {
-//                 returnedAppointments = storedAppointments[i];
-//                 details = returnedAppointments.details;
-//                 timeIndex = returnedAppointments.time;
-//                 timeIndex = timeIndex.replace(":00", '');
-//                 if (details !== null) {
-//                     $("#" + timeIndex).children('div').children('div').children('textarea').val(details);
-//                 }
-//             }
-//         }
-//     }
 
-//     renderAppointments();
+function checkTime() {
 
-//     for (i = 0; i <= 23; i++) {
-//         CurrentContainer = i;
-//         if (currentTime == i) {
-//             $('#' + CurrentContainer).addClass("present");
-//             $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("present");
-//         }
-//         else if (currentTime > i) {
-//             $('#' + CurrentContainer).addClass("past");
-//             $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("past");
-//         }
-//         else {
-//             $('#' + CurrentContainer).addClass("future");
-//             $('#' + CurrentContainer).children('div').children('div').children("textarea").addClass("future");
-//         }
-//     }
-// })
+    if (moment('9:00', 'HH:mm').isBefore(moment()) && moment('10:00', 'HH:mm').isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('present');
+        $('#ten-am').closest('div').addClass('future');
+        $('#eleven-am').closest('div').addClass('future');
+        $('#noon').closest('div').addClass('future');
+        $('#one-pm').closest('div').addClass('future');
+        $('#two-pm').closest('div').addClass('future');
+        $('#three-pm').closest('div').addClass('future');
+        $('#four-pm').closest('div').addClass('future');
+        $('#five-pm').closest('div').addClass('future');
+    
+    } else if (moment('10:00', "HH:mm").isBefore(moment()) && moment('11:00', "HH:mm").isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('present');
+        $('#eleven-am').closest('div').addClass('future');
+        $('#noon').closest('div').addClass('future');
+        $('#one-pm').closest('div').addClass('future');
+        $('#two-pm').closest('div').addClass('future');
+        $('#three-pm').closest('div').addClass('future');
+        $('#four-pm').closest('div').addClass('future');
+        $('#five-pm').closest('div').addClass('future');
 
+    } else if (moment('11:00', "HH:mm").isBefore(moment()) && moment('12:00', "HH:mm").isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('present');
+        $('#noon').closest('div').addClass('future');
+        $('#one-pm').closest('div').addClass('future');
+        $('#two-pm').closest('div').addClass('future');
+        $('#three-pm').closest('div').addClass('future');
+        $('#four-pm').closest('div').addClass('future');
+        $('#five-pm').closest('div').addClass('future');
+    
+    } else if (moment('12:00', "HH:mm").isBefore(moment()) && moment('13:00', "HH:mm").isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('past');
+        $('#noon').closest('div').addClass('present');
+        $('#one-pm').closest('div').addClass('future');
+        $('#two-pm').closest('div').addClass('future');
+        $('#three-pm').closest('div').addClass('future');
+        $('#four-pm').closest('div').addClass('future');
+        $('#five-pm').closest('div').addClass('future');
+   
+    } else if (moment('13:00', "HH:mm").isBefore(moment()) && moment('14:00', "HH:mm").isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('past');
+        $('#noon').closest('div').addClass('past');
+        $('#one-pm').closest('div').addClass('present');
+        $('#two-pm').closest('div').addClass('future');
+        $('#three-pm').closest('div').addClass('future');
+        $('#four-pm').closest('div').addClass('future');
+        $('#five-pm').closest('div').addClass('future');
+    
+    } else if (moment('14:00', "HH:mm").isBefore(moment()) && moment('15:00', "HH:mm").isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('past');
+        $('#noon').closest('div').addClass('past');
+        $('#one-pm').closest('div').addClass('past');
+        $('#two-pm').closest('div').addClass('present');
+        $('#three-pm').closest('div').addClass('future');
+        $('#four-pm').closest('div').addClass('future');
+        $('#five-pm').closest('div').addClass('future');
+    
+    } else if (moment('15:00', "HH:mm").isBefore(moment()) && moment('16:00', "HH:mm").isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('past');
+        $('#noon').closest('div').addClass('past');
+        $('#one-pm').closest('div').addClass('past');
+        $('#two-pm').closest('div').addClass('past');
+        $('#three-pm').closest('div').addClass('present');
+        $('#four-pm').closest('div').addClass('future');
+        $('#five-pm').closest('div').addClass('future');
+    
+    } else if (moment('16:00', "HH:mm").isBefore(moment()) && moment('17:00', 'HH:mm').isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('past');
+        $('#noon').closest('div').addClass('past');
+        $('#one-pm').closest('div').addClass('past');
+        $('#two-pm').closest('div').addClass('past');
+        $('#three-pm').closest('div').addClass('past');
+        $('#four-pm').closest('div').addClass('present');
+        $('#five-pm').closest('div').addClass('future');
+    
+    } else if (moment('17:00', 'HH:mm').isBefore(moment()) && moment('18:00', 'HH:mm').isAfter(moment())) {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('past');
+        $('#noon').closest('div').addClass('past');
+        $('#one-pm').closest('div').addClass('past');
+        $('#two-pm').closest('div').addClass('past');
+        $('#three-pm').closest('div').addClass('past');
+        $('#four-pm').closest('div').addClass('past');
+        $('#five-pm').closest('div').addClass('present');
+    
+    } else {
+        $('#nine-am').closest('div').addClass('past');
+        $('#ten-am').closest('div').addClass('past');
+        $('#eleven-am').closest('div').addClass('past');
+        $('#noon').closest('div').addClass('past');
+        $('#one-pm').closest('div').addClass('past');
+        $('#two-pm').closest('div').addClass('past');
+        $('#three-pm').closest('div').addClass('past');
+        $('#four-pm').closest('div').addClass('past');
+        $('#five-pm').closest('div').addClass('past');
+    }
+}
 
+checkTime();
 
-// $(".saveBtn").click(function () {
-//     appointText = $(this).parent('div').children('div').children('textarea').val();
-//     appointTime = $(this).parent('div').parent().attr("id");
-//     appointment = {
-//         time: appointTime,
-//         details: appointText
-//     }
-//     tempArray = JSON.parse(localStorage.getItem("appointments"));
-//     if (tempArray === null) {
-//         localStorage.setItem('appointments', JSON.stringify([{ time: appointTime, details: appointText }]));
-//     }
-//     else {
-//         tempArray.push(appointment);
-//         localStorage.setItem("appointments", JSON.stringify(tempArray));
+saveButton.click(function() {
 
-//     }
-//     $(this).parent('div').children('div').children('textarea').replaceWith($('<textarea>' + appointText.addClass("textarea") + '</textarea>'));
-// })
+    let appointment = {
+        time: $(this).parent('div').parent('div').attr('id'),
+        task: $(this).parent('div').children('div').children('textarea').val(),
+    }
+
+    let allAppointments = localStorage.getItem('allAppointments');
+
+    if (!allAppointments) {
+        allAppointments = {};
+
+    } else {
+        allAppointments = JSON.parse(allAppointments)
+    }
+
+    allAppointments[appointment.time] = appointment.task;
+
+    let newAppointment = JSON.stringify(allAppointments);
+    localStorage.setItem("allAppointments", newAppointment);
+})
+
+function renderAppointments() {
+
+    let storedAppointments = JSON.parse(localStorage.getItem('allAppointments'));
+
+    if (storedAppointments == null) {
+        return
+    }
+
+    let times = Object.keys(storedAppointments)
+
+    for (let time of times) {
+        let task = storedAppointments[time]
+        $('#' + time).find('textarea').val(task)
+    }
+
+}
+
+renderAppointments();
